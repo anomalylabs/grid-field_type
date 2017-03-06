25cadb15f1ebb54e107f91d00010cd7e13609b32
Dominique Netters <d.d.netters@gmail.com>
2017-03-06 11:59:25 +0100

    Added permissions

---
+++ resources/lang/en/permission.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'grids' => [
+        'name'   => 'Grids',
+        'option' => [
+            'write'  => 'Write',
+            'read'   => 'Read',
+            'delete' => 'Delete',
+        ],
+    ],
+];
